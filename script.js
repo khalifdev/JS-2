47c1509f150e74b79bcd61cc83ecbd9acf139bb0
khalifdev <n.khalif@yandex.ru>
2019-03-22 07:55:43 +0300

    Оптимизировал шаблон

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 function convert() {
-    // меняем ' на "
-    let text = document.getElementById('text').innerHTML;
-    let regexp1 = /\'/gm;
-    document.getElementById('text').innerHTML = text.replace(regexp1,'"');
 
-    // меняем " на ' в сокращениях
-    text = document.getElementById('text').innerHTML;
-    let regexp2 = /(\w)\"(\w)/g;
-    document.getElementById('text').innerHTML = text.replace(regexp2,"$1'$2");
+    let text = document.getElementById('text').innerHTML;
+    let regexp1 = /^\'|\'$|(\s)\'|\'(\s)|\'(\,)/gm;
+    document.getElementById('text').innerHTML = text.replace(regexp1,'$1"$2$3');
+
 
 }
